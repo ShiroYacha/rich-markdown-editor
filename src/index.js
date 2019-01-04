d762ecb09d8117250f0c91bf5c4475fe8769048b
Min Zhao <sebzhaomin@gmail.com>
2019-01-04 21:09:28 +0100

    Revert temporary hack

--- src/index.js
+++ src/index.js
@@ -322,7 +322,7 @@ class RichMarkdownEditor extends React.PureComponent<Props, State> {
                 />
               )}
             <StyledEditor
-              innerRef={this.setEditorRef}
+              ref={this.setEditorRef}
               plugins={this.plugins}
               value={this.state.editorValue}
               placeholder={placeholder}
